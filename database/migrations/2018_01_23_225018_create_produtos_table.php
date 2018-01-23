9bd0c1a105482927129349d2209d858893b1cb79
Daniel Hugo Gasparotto <daniel_hugo.gasparotto@hotmail.com>
2018-01-23 21:18:33 -0200

    Migrations

---
+++ database/migrations/2018_01_23_225018_create_produtos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_categoria')->unsigned();
+            $table->string('nome', 50);
+            $table->string('descricao');            
+            $table->float('preco', 8, 2);
+            $table->string('imagem', 100)->nullable();
+            $table->timestamps();
+            
+            $table->foreign('id_categoria')->references('id')->on('categorias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
